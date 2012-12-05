6266b76fbfaad9118907111d6ef2a25bf1254420
unknown <rvinay88@gmail.com>
2012-12-06 00:05:28 +0530

    Home Sick

---
+++ kids.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Template Name: Kids Page
+ *
+ *
+ * @package WP-Bootstrap
+ * @subpackage Default_Theme
+ * @since WP-Bootstrap 0.5
+ *
+ * Last Revised: July 16, 2012
+ */
+get_header(); ?>
+    <div class="container">
+        <div class="span12">
+
+          <center>
+            <a href="#">
+              <img src="<?php bloginfo ( 'template_url' ) ?>/img/logo.png" class="logo">
+            </a>  
+              <br>
+                 <a href="http://www.facebook.com/pages/The-s-community-of-entertainment/303645796319228">
+                    <img src="<?php bloginfo ( 'template_url' ) ?>/img/some/facebook.png" class="some">
+                </a>
+                <a href="http://www.twitter.com">
+                    <img src="<?php bloginfo ( 'template_url' ) ?>/img/some/twitter.png" class="some">
+                </a>
+          </center>
+        </div>
+    </div>
+
+<!--===========================Navbar=======================-->
+    <div class="navbar navbar-inverse navbar-relative-top">
+           <div class="navbar-inner">
+             <div class="container">
+           <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+          <?php
+           /** Loading WordPress Custom Menu  **/
+           wp_nav_menu( array(
+              'menu'            => 'main-menu',
+              'container_class' => 'nav-collapse',
+              'menu_class'      => 'nav',
+              'fallback_cb'     => '',
+              'menu_id' => 'main-menu',
+              'walker' => new Bootstrapwp_Walker_Nav_Menu()
+          ) ); ?>
+        </div>
+      </div>
+    </div>
+<!--===========================Navbar=======================-->
+
+<div class="container">
+  <div class="marketing">
+  <div class="row">
+    <!--========SPACER======--><div class="span12">
+        <!--========SPACER======-->&nbsp;
+    <!--========SPACER======--></div>
+
+    </div><!--row-->
+  <!--========SPACER======--><div class="span12">
+        <!--========SPACER======-->
+    <!--========SPACER======--></div>
+    
+<!--======================bottom row======================-->
+    <div class="row">
+        <div class="span12">
+
+            <div class="span2"></div>
+            <div class="span7 well"> <!--main content area-->
+                <div class="span2">
+                    <img src="<?php bloginfo ( 'template_url' ) ?>/img/zumba/home1.jpg" class="img-rounded img-polaroid">
+                </div>
+                <div class="span4">
+                    <p>Instructors from S community also specialize in teaching the art form DANCE to kids</p>
+
+                    <p>Schools have incorporated S community"s dance classes as a part of the curriculum
+                    We also have private classes for KIDS to learn dance.</p>
+                </div>
+
+                      
+              </div> <!--main content area-->
+
+          
+              
+
+        </div><!--span12-->
+
+    </div><!--row-->
+    <!--======================bottom row======================-->
+</div><!-- /.marketing -->
+</div>
+<?php get_footer();?>
---
+++ page-home-3-edit - bkp.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Template Name: Home Page 3rd Edit
+ *
+ *
+ * @package WP-Bootstrap
+ * @subpackage Default_Theme
+ * @since WP-Bootstrap 0.5
+ *
+ * Last Revised: July 16, 2012
+ */
+get_header(); ?>
+    <div class="container">
+        <div class="span12">
+
+          <center>
+            <a href="#">
+              <img src="<?php bloginfo ( 'template_url' ) ?>/img/logo.png" class="logo">
+            </a>  
+              <br>
+                 <a href="http://www.facebook.com/pages/The-s-community-of-entertainment/303645796319228">
+                    <img src="<?php bloginfo ( 'template_url' ) ?>/img/some/facebook.png" class="some">
+                </a>
+                <a href="http://www.twitter.com">
+                    <img src="<?php bloginfo ( 'template_url' ) ?>/img/some/twitter.png" class="some">
+                </a>
+          </center>
+        </div>
+    </div>
+
+
+
+<div class="container">
+  
+
+
+
+  <div class="row-fluid well">
+
+<div class="span12">
+
+              <!--le awesome flex slider-->
+        <div class="flexslider2">
+              <ul class="slides">
+                <li>
+                  <img src="<?php bloginfo ( 'template_url' ) ?>/img/home/slides/1.jpg" class="img-rounded" />
+                </li>
+                <li>
+                  <img src="<?php bloginfo ( 'template_url' ) ?>/img/home/slides/2.jpg" class="img-rounded" />
+                </li>
+                <li>
+                  <img src="<?php bloginfo ( 'template_url' ) ?>/img/home/slides/3.jpg" class="img-rounded" />
+                </li>
+                <li>
+                  <img src="<?php bloginfo ( 'template_url' ) ?>/img/home/slides/4.jpg" class="img-rounded" />
+                </li>
+              </ul>
+            </div><!--Le Awesome slider ends :( -->
+</div>
+<div class="span12">
+
+
+      <ul class="ch-grid">
+          <li>
+            <a href="<?php bloginfo ( 'wpurl' ) ?>/instructors/">
+              <div class="ch-item ch-img-1">
+                  <div class="ch-info">
+                      <h3>Zumba, Bokwa, Bollywood, Kids</h3>
+                      
+                  </div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="<?php bloginfo ( 'wpurl' ) ?>/performers/">
+              <div class="ch-item ch-img-2">
+                  <div class="ch-info">
+                      <h3>Contemporary, Jazz, Hip hop, Salsa, Bollywood and freestyle </h3>
+                     
+                  </div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="<?php bloginfo ( 'wpurl' ) ?>/show-managers/">
+              <div class="ch-item ch-img-3">
+                  <div class="ch-info">
+                      <h3>Concept, Preproduction, Postproduction, Design, Sound, lights  </h3>
+                     
+                  </div>
+              </div>
+            </a>
+          </li>
+      </ul>
+
+      <ul class="ch-grid">
+            <li>
+              <a href="<?php bloginfo ( 'wpurl' ) ?>/dj/">
+                <div class="ch-item ch-img-4">
+                  <div class="ch-info">
+                      <h3>House, techno, hip hop, club, electro</h3>
+                      
+                    </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href="<?php bloginfo ( 'wpurl' ) ?>/choreographers/">
+                <div class="ch-item ch-img-5">
+                    <div class="ch-info">
+                        <h3>Schools, Colleges, Corporates, Competitions, flash mobs</h3>
+                      
+                    </div>
+                </div>
+              </a>
+            </li>
+        </ul>
+
+      </div><!--span12-->
+
+        </div><!--row fluid-->
+  
+    
+
+
+    
+
+<?php get_footer();?>
+</div>
+
